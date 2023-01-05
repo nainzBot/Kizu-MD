fe1a5f3ffd1e526262b10db885e71d7d2fad3cf0
nainzBot <121393944+nainzBot@users.noreply.github.com>
2023-01-05 14:05:44 +0700

    yy
    yy

--- plugins/info-listpc.js
+++ plugins/info-listpc.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn }) => {
 let pc = Object.entries(await conn.chats)
 let niorg = pc.filter(([jid]) => jid.endsWith('@s.whatsapp.net'))
-let thumbListpc = `https://telegra.ph/file/6db207196807bcf1ca1c5.jpg`
+let thumbListpc = `https://telegra.ph/file/67efab3c53f2a7c6e5ce3.jpg`
 let txt = ''
     for (let [jid] of niorg)
 txt += `${htjava} ${await conn.getName(jid)}\n${dmenub} ${'@' + jid.replace(/@.+/, '')}\n${cmenua}\n\n`
