af5b291ce86107e8fdcf2f024dc8623d7353ac32
nainzBot <121393944+nainzBot@users.noreply.github.com>
2023-01-05 14:01:05 +0700

    yy
    yy

--- plugins/gczyko.js
+++ plugins/gczyko.js
@@ -4,12 +4,12 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
   
     
     conn.sendButtonDoc(text, wm, 'ᴡᴇʟᴄᴏᴍᴇ', fkontak, { contextInfo: { externalAdReply: { showAdAttribution: false,
-    mediaUrl: 'https://instagram/wannzx_133',
+    mediaUrl: 'https://instagram/dikaaxyz_',
     mediaType: 2, 
     description: sgc,
     title: '≪ ᴄʟɪᴄᴋ ʜᴇʀᴇ ⋟',
     body: wm,
-    thumbnail: await(await fetch("https://telegra.ph/file/52866b574c930d17f0c1c.jpg")).buffer(),
+    thumbnail: await(await fetch("https://telegra.ph/file/67efab3c53f2a7c6e5ce3.jpg")).buffer(),
     sourceUrl: sgc
      }}
   })*/
@@ -24,12 +24,12 @@ let str = `
 `
 
 conn.sendButtonDoc(m.chat, str, wm,' ᴏᴋᴇ ','huuu', m, { contextInfo: { externalAdReply: { showAdAttribution: false,
-    mediaUrl: 'https://instagram/kabulsaputra21',
+    mediaUrl: 'https://instagram/dikaaxyz_',
     mediaType: 2, 
     description: sgc,
     title: '≪ Jᴏɪɴ Sɪɴɪ Cᴜʏ ⋟',
     body: wm,
-    thumbnail: await(await fetch("https://telegra.ph/file/c653d0920d9f267dda58d.jpg")).buffer(),
+    thumbnail: await(await fetch("https://telegra.ph/file/67efab3c53f2a7c6e5ce3.jpg")).buffer(),
     sourceUrl: sgc
   }
   } }) 
